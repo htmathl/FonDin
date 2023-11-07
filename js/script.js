af8e82034acf38340110f4c584b36e4a3dbd8dc6
htmathl <matheus.domingues126@gmail.com>
2023-11-07 00:41:54 -0300

    Testes de animações

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let fundoPreto = document.getElementById('fundoPreto');
 let botao = document.getElementById('botao');
-let desativado = true;
+let desativadoFAB, desativadoCredRec, desativadoCredInd = true;
 
 let mais = document.getElementById('mais');
 let menos = document.getElementById('menos');
@@ -10,7 +10,16 @@ let pathBotao = document.querySelectorAll('.path-botao');
 
 let credMes = document.getElementById('credmes');
 
+let cbCreditoRecorrente = document.querySelector('.credito-recorrente');
+let lgdCreditoDia = document.querySelector('.dia-cred');
+
+let divCredIndefinido = document.querySelector('.cb-cred-indefinido');
+let cbCredIndefinido = document.querySelector('.indefinido-cred');
+
 let inputValorCredito = document.getElementById('valor');
+
+let divEntradasCreditos = document.querySelector('#entradas-creditos');
+let btnVoltarAdicionarCred = document.getElementById('btn-voltar-cred'); 
 
 function formatarMoeda() {
     var valor = inputValorCredito.value;
@@ -25,7 +34,10 @@ function formatarMoeda() {
     }
 
     inputValorCredito.value = valor;
-    if(valor == 'NaN') inputValorCredito.value = '';
+    if(valor == 'NaN') {
+        inputValorCredito.value = '';
+        inputValorCredito.classList.remove('input-focus');
+    }
 }
 
 inputValorCredito.addEventListener('input', () => {
@@ -36,7 +48,7 @@ inputValorCredito.addEventListener('input', () => {
 });
 
 botao.addEventListener('click', () => {
-    if(desativado) {
+    if(desativadoFAB) {
         pathBotao.forEach(e => {
             e.style.stroke = '#FFF2D8';
         });
@@ -57,7 +69,7 @@ botao.addEventListener('click', () => {
         cifrao.style.bottom = '20vh';
         cifrao.style.transform = 'translate(50%) rotate(45deg)';
 
-        desativado = false;
+        desativadoFAB = false;
     } else {
         pathBotao.forEach(e => {
             e.style.stroke = '#113946';
@@ -79,7 +91,42 @@ botao.addEventListener('click', () => {
         cifrao.style.bottom = '5vh';
         cifrao.style.transform = 'translate(50%) rotate(0)';
 
-        desativado = true;
+        desativadoFAB = true;
     }
 });
 
+cbCreditoRecorrente.addEventListener('click', () => {
+    if(desativadoCredRec) {
+        cbCreditoRecorrente.classList.add('credito-recorrente-hover');
+        lgdCreditoDia.innerHTML = 'Todo dia';
+        desativadoCredRec = false;
+    } else {
+        cbCreditoRecorrente.classList.remove('credito-recorrente-hover');
+        lgdCreditoDia.innerHTML = 'Dia';
+        desativadoCredRec = true;
+    }
+});
+
+divCredIndefinido.addEventListener('click', () => {
+    if(desativadoCredInd) {
+        cbCredIndefinido.classList.add('indefinido-cred-hover');
+        desativadoCredInd = false;
+    } else {
+        cbCredIndefinido.classList.remove('indefinido-cred-hover');
+        desativadoCredInd = true;
+    }
+});
+
+mais.addEventListener('click', () => {
+    mais.classList.add('mais-active');
+    setTimeout(() => {
+        divEntradasCreditos.classList.add('entradas-creditos-active');
+    }, 500);
+});
+
+btnVoltarAdicionarCred.addEventListener('click', () => {
+    divEntradasCreditos.classList.remove('entradas-creditos-active');
+    setTimeout(() => {
+        mais.classList.remove('mais-active');
+    }, 1000);
+});
