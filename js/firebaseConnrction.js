4a90731e9c370b317a5dc652e8752996d60bb985
Matheus Ferreira Domingues <57194557+htmathl@users.noreply.github.com>
2024-04-18 17:31:57 +0000

    Adicionar integração com firebase

---
+++ js/firebaseConnrction.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCwQZRSyB8MAydnf2cXAS7MH8nYDEXjSzM",
+  authDomain: "fondin-1.firebaseapp.com",
+  projectId: "fondin-1",
+  storageBucket: "fondin-1.appspot.com",
+  messagingSenderId: "840972180581",
+  appId: "1:840972180581:web:f66506c9e885768adb3a8c",
+  measurementId: "G-77T25H9BRC"
+};
+
+firebase.initializeApp(firebaseConfig);
---
+++ js/login.js
@@ -0,0 +1,1 @@
+firebase.auth().signInWithEmailAndPassword("miau@lars.com", "miau123").then(response => console.log('success', response)).catch(error => console.log('Error', error));
